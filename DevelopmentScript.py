beac98b272680502bc7dc7fa3adb94f647cff830
Gil Liran <Gil.Liran@e.braude.ac.il>
2023-07-31 12:03:46 +0300

    Changes to docker file. Problems not fixed

--- DevelopmentScript.py
+++ DevelopmentScript.py
@@ -1,24 +1,29 @@
 import subprocess
 import os
 
-
 # Define the path to the CMake executable
 cmake_executable = r'C:\Program Files\CMake\bin\cmake.exe'
 
 # Define the path to the CMakeLists.txt file
 cmake_file_path = r"C:\Users\USER\OneDrive - ort braude college of engineering\Desktop\MyProject_1\CMakeLists.txt"
 
-# Define the build directory
-build_directory = r'C:\Users\USER\OneDrive - ort braude college of engineering\Desktop\MyProject_1\build'
+# Define the build directory (use absolute path)
+build_directory = os.path.abspath('MyProject_1/build')
+
+# Create the build directory if it doesn't exist
+os.makedirs(build_directory, exist_ok=True)
 
 # Run the CMake command to generate the build files
-# cmake_command = [cmake_executable, cmake_file_path, '-B', build_directory]
 cmake_command = [cmake_executable, cmake_file_path, '-B', build_directory]
 subprocess.run(cmake_command, check=True)
 
 # Change the current working directory to the build directory
 os.chdir(build_directory)
 
-# Run the build command (e.g., make)
-build_command = ['make']
+# Run the build command (use absolute path for 'make')
+build_command = ['cmake', cmake_file_path]
 subprocess.run(build_command, check=True)
+
+#cmake [options] -S <path-to-source> -B <path-to-build>
+
+#subprocess.run('make', 'Encripter', check=True)
--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,18 @@
 # Use an official Python runtime as the base image
 FROM python:3.9
 
-# Set the working directory inside the container
-WORKDIR /app
-
 # Install CMake
 RUN apt-get update && apt-get install -y cmake
 
+# Set the working directory inside the container
+WORKDIR /app
+
 # Copy the entire project code to the working directory
 COPY . .
+
+# Set the full path to the CMake executable within the container using ARG
+ARG cmake_path="C:\\Program Files\\CMake\\bin"
+ENV PATH="${cmake_path}:${PATH}"
 
 # Set the command to run when the container starts
 CMD ["python", "DevelopmentScript.py"]
